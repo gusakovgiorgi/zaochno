12a3e5ec5156cebae6928b56921e4e02f8eedd2b
Gusakov Giorgi <gg@mo-apps.com>
2017-05-25 18:05:28 +0300

    done filtering dialog

--- app/src/main/java/ru/zaochno/zaochno/dialogs/FilterDialogFragment.java
+++ app/src/main/java/ru/zaochno/zaochno/dialogs/FilterDialogFragment.java
@@ -4,11 +4,15 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarFinalValueListener;
 import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
 
 import ru.zaochno.zaochno.R;
@@ -24,42 +28,47 @@ public class FilterDialogFragment extends DialogFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setStyle(DialogFragment.STYLE_NORMAL,android.R.style.Theme_Holo_Light_Dialog);
+        setStyle(DialogFragment.STYLE_NO_TITLE, android.R.style.Theme_Holo_Light_Dialog);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Set title for this dialog
-        getDialog().setTitle("My Dialog Title");
 
         View v = inflater.inflate(R.layout.fragment_dialog_item, container, false);
 
         // get seekbar from view
         final CrystalRangeSeekbar rangeSeekbar = (CrystalRangeSeekbar) v.findViewById(R.id.dialogFragmentRangeSeekbarId);
 
-//// get min and max text view
-//        final TextView tvMin = (TextView) rootView.findViewById(R.id.textMin1);
-//        final TextView tvMax = (TextView) rootView.findViewById(R.id.textMax1);
-//
-//// set listener
-//        rangeSeekbar.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
-//            @Override
-//            public void valueChanged(Number minValue, Number maxValue) {
-//                tvMin.setText(String.valueOf(minValue));
-//                tvMax.setText(String.valueOf(maxValue));
-//            }
-//        });
-//
-//// set final value listener
-//        rangeSeekbar.setOnRangeSeekbarFinalValueListener(new OnRangeSeekbarFinalValueListener() {
-//            @Override
-//            public void finalValue(Number minValue, Number maxValue) {
-//                Log.d("CRS=>", String.valueOf(minValue) + " : " + String.valueOf(maxValue));
-//            }
-//        });
-//        View tv = v.findViewById(R.id.text);
-//        ((TextView)tv).setText("This is an instance of MyDialogFragment");
+// get min and max text view
+        final TextView tvMin = (TextView) v.findViewById(R.id.dialogFragmentRangeSeekbarLeftTextViewId);
+        final TextView tvMax = (TextView) v.findViewById(R.id.dialogFragmentRangeSeekbarRightTextViewId);
+
+// set listener
+        rangeSeekbar.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number minValue, Number maxValue) {
+                tvMin.setText(String.valueOf(minValue));
+                tvMax.setText(String.valueOf(maxValue));
+            }
+        });
+
+// set final value listener
+        rangeSeekbar.setOnRangeSeekbarFinalValueListener(new OnRangeSeekbarFinalValueListener() {
+            @Override
+            public void finalValue(Number minValue, Number maxValue) {
+                Log.d("CRS=>", String.valueOf(minValue) + " : " + String.valueOf(maxValue));
+            }
+        });
+
+        View closeImg=v.findViewById(R.id.dialogFragmentCloseImageViewId);
+        closeImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
         return v;
     }
 
